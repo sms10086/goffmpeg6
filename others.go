b697e0f2e2093fe7d6f151156618edd5d518674c
unknown <sms10086@hotmail.com>
2024-03-17 13:33:31 +0800

    upate

---
+++ others.go
@@ -0,0 +1,67 @@
+package goffmpeg6
+
+// libavcodec/avcodec.h
+type AVCodecInternal struct {
+
+}
+
+// libavcodec/d3d11va.h
+type HANDLE uintptr
+type ID3D11VideoDecoder struct {
+}
+type ID3D11VideoContext struct {
+}
+type D3D11_VIDEO_DECODER_CONFIG struct {
+}
+type ID3D11VideoDecoderOutputView struct {
+}
+
+// libcodec/dxva2.h
+type IDirectXVideoDecoder struct {
+}
+type DXVA2_ConfigPictureDecode struct {
+}
+type LPDIRECT3DSURFACE9 struct {
+}
+
+// libavfilter/avfilter.h
+type AVFilterCommand struct {
+}
+
+// libavformat/avformat.h
+type AVIndexEntry struct {
+	Pos int64
+	Timestamp int64
+	Flags_and_size int32
+	Min_distance int32
+}
+
+type URLContext struct {
+}
+
+// libavutil/hwcontext_d3d1va.h
+type ID3D11Device struct {
+}
+type ID3D11DeviceContext struct {
+}
+type ID3D11VideoDevice struct {
+}
+type ID3D11Texture2D struct {
+}
+
+// libavcodec/videotoolbox.h
+type VTDecompressionSessionRef uintptr
+type OSType int32
+type CMVideoFormatDescriptionRef uintptr
+
+// libavcodec/xvmc.h
+type XvMCMacroBlock struct {
+}
+type XvMCSurface struct {
+}
+
+// libutil/hwcontext_dxva2.h
+type IDirect3DDeviceManager9 struct {
+}
+type IDirect3DSurface9 struct {
+}
